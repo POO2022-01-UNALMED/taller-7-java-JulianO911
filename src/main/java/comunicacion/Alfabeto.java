bfdf38c50a58c426d41c845ffbf55ffe01c4740f
Julian O <juospinam@unal.edu.co>
2022-05-11 01:05:24 -0500

    solucion

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,46 @@
+package comunicacion;
+
+import java.util.ArrayList;
+
+public class Alfabeto extends Pictograma {
+	private String[] letras;
+	private String interpretacion;
+	
+	public Alfabeto(String origen,String[] letras,String interpretacion) {
+		super(origen);
+		this.letras = letras;
+		this.interpretacion = interpretacion;
+	}
+
+	public String[] getLetras() {
+		return letras;
+	}
+	
+	public void setLetras(String[] letras) {
+		this.letras = letras;
+	}
+	
+	
+	public void setLInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	
+	String interpretacion () {
+		return interpretacion;
+	}
+	
+	public String toString() {
+		String retorno = "";
+		for(String letra:letras) {
+			retorno += letra+", ";
+		}
+		return retorno;
+	}
+	
+	public int cantidadLetras() {
+		return letras.length;
+	}
+
+	
+	
+}
---
+++ src/main/java/comunicacion/Fabula.java
@@ -0,0 +1,38 @@
+package comunicacion;
+
+public class Fabula extends Escrito{
+    private String enseñanza;
+    private String interpretacion;
+
+    public Fabula(String origen, String titulo,String autor,int paginas,String enseñanza, String interpretacion) {
+        super(origen,titulo,autor,paginas);
+        this.enseñanza = enseñanza;
+        this.interpretacion = interpretacion;
+    }
+    
+    public String getEnseñanza() {
+        return enseñanza;
+    }
+
+    public void setEnseñanza(String enseñanza) {
+        this.enseñanza = enseñanza;
+    }
+
+    String Interpretacion() {
+        return interpretacion;
+    }
+
+    public void setInterpretacion(String interpretacion) {
+        this.interpretacion = interpretacion;
+    }
+    
+	public String toString() {
+		String retorno = "";
+
+		return retorno;
+	}
+	
+	int palabrasTotales(int palabrasPagina) {
+		
+	}
+}
